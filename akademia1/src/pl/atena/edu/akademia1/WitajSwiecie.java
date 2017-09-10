325bc8487a4ffcbfb0c244833ad0b8e0cfd82d6f
globus1987 <31816025+globus1987@users.noreply.github.com>
2017-09-10 10:48:52 +0200

    start 1

---
+++ akademia1/src/pl/atena/edu/akademia1/WitajSwiecie.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pl.atena.edu.akademia1;
+
+/**
+ * @author Arkadiusz
+ *
+ */
+public class WitajSwiecie {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
