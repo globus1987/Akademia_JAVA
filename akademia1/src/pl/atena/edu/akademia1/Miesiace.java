373a3a2a9de5d2b5b3c4279c177e524dc9754a52
globus1987 <31816025+globus1987@users.noreply.github.com>
2017-09-10 16:07:34 +0200

    miesiace

---
+++ akademia1/src/pl/atena/edu/akademia1/Miesiace.java
@@ -0,0 +1,54 @@
+package pl.atena.edu.akademia1;
+
+import java.util.Scanner;
+
+public class Miesiace {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static String miesiac(int liczba) {
+		switch (liczba) {
+		case 1:
+			return "Styczeñ";
+		case 2:
+			return "Luty";
+		case 3:
+			return "Marzec";
+		case 4:
+			return "Kwiecieñ";
+		case 5:
+			return "Maj";
+		case 6:
+			return "Czerwiec";
+		case 7:
+			return "Lipiec";
+		case 8:
+			return "Sierpieñ";
+		case 9:
+			return "Wrzesieñ";
+		case 10:
+			return "Pazdziernik";
+		case 11:
+			return "Listopad";
+		case 12:
+			return "Grudzieñ";
+		default:
+			return "nie ma miesiêcy wiêcej ni¿ 12!!!";
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int miesiacLiczba = 0;
+		System.out.println("podaj nr miesi¹ca");
+		String mies = scanner.nextLine();
+		try {
+			miesiacLiczba = Integer.valueOf(mies);
+		} catch (Exception e) {
+			System.out.println("to nie numer miesiaca!!!");
+			return;
+		}
+		String miech = miesiac(miesiacLiczba);
+		System.out.println(miech);
+	}
+
+}
--- akademia1/src/pl/atena/edu/akademia1/PoleProstokata.java
+++ akademia1/src/pl/atena/edu/akademia1/PoleProstokata.java
@@ -17,12 +17,14 @@ public class PoleProstokata {
 	public static final int MAX = 10;
 
 	public static boolean sprawdz(double liczba, String komunikat) {
-		if (liczba <= MIN || liczba > MAX) {
+		if (liczba < MIN || liczba > MAX) {
 			System.out.println("bok " + komunikat + " jest spoza zakresu " + MIN + "-" + MAX);
 			return false;
 		}
 		return true;
 	}
+
+
 
 	public static void main(String[] args) {
 		System.out.println("Podaj d³ugoœæ boku A:");
@@ -31,16 +33,58 @@ public class PoleProstokata {
 		System.out.println("Podaj d³ugoœæ boku B:");
 		String db = scanner.nextLine();
 		boolean resultA = true, resultB = true;
-		double dlugoscA2 = Double.valueOf(da), dlugoscB2 = Double.valueOf(db);
+		double dlugoscA2 = 0, dlugoscB2 = 0;
+		try {
+			dlugoscA2 = Double.valueOf(da);
 
-		resultA = sprawdz(dlugoscA2, "A");
-		resultB = sprawdz(dlugoscB2, "B");
-		// oba boki musz¹ spe³niaæ warunek wartoœci granicznych
+		} catch (Exception e) {
+			System.out.println("B³êdny bok A");
+			resultA = false;
+		}
+		try {
+			dlugoscB2 = Double.valueOf(db);
+
+		} catch (Exception e) {
+			System.out.println("B³êdny bok B");
+			resultB = false;
+		}
 		if (!resultA || !resultB) {
 			return;
 		}
-		double Pole2 = dlugoscA2 * dlugoscB2;
-		System.out.println("D³ugoœæ A:" + dlugoscA2 + ",d³ugoœæ B:" + dlugoscB2 + ", Pole:" + Pole2);
+		resultA = sprawdz(dlugoscA2, "A");
+		resultB = sprawdz(dlugoscB2, "B");
+		// oba boki musz¹ spe³niaæ warunek wartoœci granicznych
+		if (!resultA && !resultB) {
+			System.out.println("popraw dane A i B");
+			return;
+		} else if (!resultA && resultB) {
+			System.out.println("popraw dane A");
+			return;
+		} else if (resultA && !resultB) {
+			System.out.println("popraw dane B");
+			return;
+		} else {
+			double Pole2 = dlugoscA2 * dlugoscB2;
+			System.out.println("D³ugoœæ A:" + dlugoscA2 + ",d³ugoœæ B:" + dlugoscB2 + ", Pole:" + Pole2);
+		}
+		String testowa = "raz";
+		switch (da) {
+		case "1":
+			testowa = "jeden";
+			break;
+		case "2":
+			testowa = "dwa";
+			break;
+		case "3":
+			testowa = "trzy";
+			break;
+		default:
+			testowa = "nie wiem ile to";
+			break;
+		}
+		System.out.println(testowa);
+
+	
 	}
 
 }
