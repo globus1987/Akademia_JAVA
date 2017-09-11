968b4a9ea51133a280849f983759adb8ce4307f0
globus1987 <31816025+globus1987@users.noreply.github.com>
2017-09-11 13:49:22 +0200

    testtool start

---
+++ TestTool/src/pl/atena/edu/testtool/TestTool.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pl.atena.edu.testtool;
+
+/**
+ * @author Arkadiusz
+ *
+ */
+public class TestTool {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
