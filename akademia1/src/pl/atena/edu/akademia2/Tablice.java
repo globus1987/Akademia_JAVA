eb31dd6027b2cec7687467e0f8e48dfed9b58ff8
globus1987 <31816025+globus1987@users.noreply.github.com>
2017-09-24 14:21:24 +0200

    tablice wielowymiarowe

--- akademia1/src/pl/atena/edu/akademia2/Tablice.java
+++ akademia1/src/pl/atena/edu/akademia2/Tablice.java
@@ -1,6 +1,7 @@
 package pl.atena.edu.akademia2;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Tablice {
 
@@ -34,23 +35,37 @@ public class Tablice {
 
 		int[] tabliczka = new int[10];
 		int value = 9;
-		for (int z = 0; z < tabliczka.length; z++,value--) {
+		for (int z = 0; z < tabliczka.length; z++, value--) {
 			tabliczka[z] = value;
 			System.out.printf("Dane[%d] = %d%n", z, tabliczka[z]);
 		}
-		
-		System.out.println("tabliczka "+Arrays.toString(tabliczka));
-		int[] tabliczkaNowa=Arrays.copyOf(tabliczka, tabliczka.length+5);
+
+		System.out.println("tabliczka " + Arrays.toString(tabliczka));
+		int[] tabliczkaNowa = Arrays.copyOf(tabliczka, tabliczka.length + 5);
 		int[] tabliczkaref = tabliczka;
-		System.out.println("tabliczkaref "+Arrays.toString(tabliczkaref));
-		tabliczkaref[2]=10000;
-		System.out.println("tabliczka "+Arrays.toString(tabliczka));
-		System.out.println("tabliczkaref "+Arrays.toString(tabliczkaref));
-
-		System.out.println("tabliczkanowa "+Arrays.toString(tabliczkaNowa));
+		System.out.println("tabliczkaref " + Arrays.toString(tabliczkaref));
+		tabliczkaref[2] = 10000;
+		System.out.println("tabliczka " + Arrays.toString(tabliczka));
+		System.out.println("tabliczkaref " + Arrays.toString(tabliczkaref));
+		System.out.println("tabliczkanowa " + Arrays.toString(tabliczkaNowa));
+		//tablice wielowymiarowe
+		int[] tab = { 4, 12, 58, 45, 5, 23, 52, 1, 44 };
+		Arrays.sort(tab);
+		System.out.println(Arrays.toString(tab));
+		int[] tab2 = new int[10];
+		Random generator = new Random();
+		Arrays.fill(tab2, generator.nextInt(11));
+		System.out.println(Arrays.toString(tab2));
 		
-
-
+		Integer[][] tab3= {{1,2,3,4},{4,3,2,1},{9,8,7,4}};
+		for (Integer[] kolumna:tab3) {
+			for (Integer element:kolumna) {
+				System.out.print(element+" ");
+			}
+			System.out.println("\n");
+		}
+		
+		
+		
 	}
-
 }
